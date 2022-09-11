fac468b96dd25da0236a89998c8cd22e69d6695d
Redzic <yusufredzic@gmail.com>
2022-09-11 15:15:05 -0500

    Simplify some more code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::io;
+use std::io::{BufRead, BufReader, Read, Seek, SeekFrom};
 use std::mem::size_of;
 
 const JPEG_MARKER: [u8; 2] = [0xff, 0xd8];
@@ -31,6 +32,22 @@ fn get_jpeg_segment_name(marker: u16) -> &'static str {
     }
 }
 
+/// Reads unsigned short in big-endian format
+fn read_u16(reader: &mut BufReader<File>) -> io::Result<u16> {
+    let mut buf = [0; 2];
+    reader.read_exact(&mut buf)?;
+
+    Ok(u16::from_be_bytes(buf))
+}
+
+/// Reads byte
+fn read_u8(reader: &mut BufReader<File>) -> io::Result<u8> {
+    let mut buf = [0];
+    reader.read_exact(&mut buf)?;
+
+    Ok(buf[0])
+}
+
 fn main() -> Result<(), std::io::Error> {
     let mut reader = BufReader::new(File::open("./profile.jpg")?);
 
@@ -59,10 +76,39 @@ fn main() -> Result<(), std::io::Error> {
                 // end of the image.
                 reader.seek(SeekFrom::End(-(size_of::<u16>() as i64)))?;
             }
+            0xffe0 => {
+                let len = read_u16(&mut reader)?;
+
+                let mut null_str = Vec::new();
+
+                // TODO read len-2 bytes upfront, and search that area instead
+                // of doing it this pretty garbage way
+
+                // read null-terminated string
+                let n_read = reader.read_until(0, &mut null_str)?;
+                assert!(
+                    n_read <= len as usize - size_of::<u16>(),
+                    "Invalid length after marker in Application Default Header"
+                );
+                // TODO technically not invalid length, but actually the string info
+                // or whatever is just too long
+
+                // TODO make read<N> helper function
+                let v_maj = read_u8(&mut reader)?;
+                let v_min = read_u8(&mut reader)?;
+
+                let units = read_u8(&mut reader)?;
+
+                let dx = read_u16(&mut reader)?;
+                let dy = read_u16(&mut reader)?;
+
+                // Thumbnail information
+                let tx = read_u8(&mut reader)?;
+                let ty = read_u8(&mut reader)?;
+            }
             _ => {
                 // read another BE u16, which indicates the length
-                reader.read_exact(&mut buf)?;
-                let len = u16::from_be_bytes(buf);
+                let len = read_u16(&mut reader)?;
 
                 // The readed length includes the size of itself,
                 // but since we advanced the reader 2 bytes to actually
